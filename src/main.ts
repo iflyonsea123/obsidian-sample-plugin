90b827c9c37a67a7af1eb0cff7d6c8f198db0818
wanggang <wanggang@hj.com>
2026-05-21 14:22:25 +0800

    将 LaTeX 数学符号转换为 Obsidian 原生格式

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin} from 'obsidian';
+import {App, Editor, MarkdownView, Notice, Plugin} from 'obsidian';
 import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
 
 // Remember to rename these classes and interfaces!
@@ -9,68 +9,48 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		// Replace math delimiters icon in the left ribbon
+		this.addRibbonIcon('function-square', 'Convert Math Delimiters', (evt: MouseEvent) => {
+			const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+			if (activeView) {
+				const editor = activeView.editor;
+				this.convertMathDelimiters(editor);
+			} else {
+				new Notice('No active Markdown view to convert.');
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
+
+		// Command to replace math delimiters
 		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
+			id: 'convert-math-delimiters',
+			name: 'Convert Math Delimiters (LaTeX to Obsidian)',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
+				this.convertMathDelimiters(editor);
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-
 	}
 
 	onunload() {
+	}
+
+	convertMathDelimiters(editor: Editor) {
+		const text = editor.getValue();
+		
+		let newText = text
+			.replace(/\\\[/g, () => '$$')
+			.replace(/\\\]/g, () => '$$')
+			.replace(/\\\(/g, () => '$')
+			.replace(/\\\)/g, () => '$');
+
+		if (text !== newText) {
+			editor.setValue(newText);
+			new Notice('Math delimiters converted successfully!');
+		} else {
+			new Notice('No math delimiters found to convert.');
+		}
 	}
 
 	async loadSettings() {
@@ -82,18 +62,3 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
